fe2b64a6db6d792af81cad7cd80d29416e396656
fedesoavi <f.soavi@osl.it>
2023-02-08 17:44:13 +0100

    added firewall check

--- Funzionalita_HW.ps1
+++ Funzionalita_HW.ps1
@@ -474,6 +474,9 @@ Function main {
                 #[D] Copio dati di un dsn dentro init servizio
                 Copy-DsnToInit                
             }
+            F{
+                Get-NetFirewallProfile -PolicyStore ActiveStore
+            }
             X {    
                 #[X] chiude script
                 #Garbage collection
