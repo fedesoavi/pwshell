b46ee2aa923fd9bc0f8d78acb1f483df04444c69
fedesoavi <f.soavi@osl.it>
2023-02-15 17:26:00 +0100

    final

--- Funzionalita_HW.ps1
+++ Funzionalita_HW.ps1
@@ -471,9 +471,6 @@ Function main {
                 #[D] Copio dati di un dsn dentro init servizio
                 Copy-DsnToInit                
             }
-            F{
-                Get-NetFirewallProfile -PolicyStore ActiveStore
-            }
             X {    
                 #[X] chiude script
                 #Garbage collection
