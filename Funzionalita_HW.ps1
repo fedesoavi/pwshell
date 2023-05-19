ab370ae097677a139e0d1c544aa3963327c036a8
fedesoavi <104622242+fedesoavi@users.noreply.github.com>
2023-05-19 17:53:08 +0200

    Update Funzionalita_HW.ps1
    removed dot from beginning

--- Funzionalita_HW.ps1
+++ Funzionalita_HW.ps1
@@ -1,4 +1,3 @@
-﻿
 if (!( [Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     Exit
