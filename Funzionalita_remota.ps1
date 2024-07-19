f4540a185456d36b163c46700c2a44750456cb68
fede0521 <fede0521@gmail.com>
2024-07-19 16:13:56 +0200

    fixed admin

--- Funzionalita_remota.ps1
+++ Funzionalita_remota.ps1
@@ -1,4 +1,1 @@
-Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/fedesoavi/pwshell/main/Funzionalita_HW.ps1')
-
-(new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/fedesoavi/pwshell/main/Funzionalita_HW.ps1','local.ps1')
-./local.ps1 "test"
+Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/fedesoavi/pwshell/main/Funzionalita_HW.ps1')
